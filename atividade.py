abae64540e8ce425ca14c08c8996ce7a32e82cf9
Isaac 204 <126293048+xtrinksx@users.noreply.github.com>
2025-05-06 11:44:34 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,4 @@
+si = float(input("digite o saldo inicial: ", ))
+r1 = float(input("digite o valor da primeira retirada: ", ))
+sri = float(si - r1)
+print(f"seu saldo após a primeira retirada é: ", sr1)
